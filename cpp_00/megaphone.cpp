3a9ab42926173dfad075cb5376ff9c3eac0f2c9e
Jazmin Acosta <jaezatc@gmail.com>
2026-07-12 14:34:51 +0200

    cpp_00 ex_00 done

---
+++ cpp_00/megaphone.cpp
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariacos <mariacos@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/12 09:39:30 by mariacos          #+#    #+#             */
+/*   Updated: 2026/07/12 14:34:10 by mariacos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+void lowerToUpper(const char **str)
+{
+	int i;
+	int j;
+	char c;
+
+	i = 1;
+	j = 0;
+	while(str[i])
+	{
+		j = 0;
+		while(str[i][j])
+		{
+			if(str[i][j] >= 'a' && str[i][j] <= 'z')
+			{
+				c = str[i][j] - 32;
+				std::cout << c;
+			}
+			else
+				std::cout << str[i][j];
+			j++;
+		}
+		i++;
+	}
+}
+
+int main(int argc, const char **argv)
+{
+	const char *str;
+
+	str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+	if (argc > 1)
+		lowerToUpper(argv);
+	else
+		std::cout << str;
+	return 0;
+}
