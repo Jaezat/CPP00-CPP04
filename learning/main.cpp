3af2e5fdd9d369f0668fa993f015acc77289dfbd
Mariacos <mariacos@student.42.lisboa.com>
2026-08-06 16:08:35 +0100

    practicing

--- learning/main.cpp
+++ learning/main.cpp
@@ -16,19 +16,30 @@ public:
 		OwnerName = ownerName;
 		SubscriberCount = 0;
 	}
+	// creating a method/function
+	void GetInfo()
 };
 
 int main()
 {
 	YoutubeChannel mariacos("Minombre", "perris");
+	mariacos.PublishedVideoTitles.push_back("C++ for beginners");
+	YoutubeChannel mariacos2("Hello", "perra");
+
 	mariacos.PublishedVideoTitles.push_back("hey");
 	mariacos.PublishedVideoTitles.push_back("oi");
 	mariacos.PublishedVideoTitles.push_back("holi");
 
 	cout << "Name: " << mariacos.Name << endl;
+	cout << "Ownername: " << mariacos.OwnerName << endl;
+	for (string Videotitle : mariacos.PublishedVideoTitles)
+	{
+		cout << Videotitle << endl
+	}
+
+	
 
 
-	YoutubeChannel mariacos2("Hello", "perra");
 
 	
 }
