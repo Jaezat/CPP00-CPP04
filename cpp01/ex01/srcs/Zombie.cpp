c9fb29dd83870a69ffc345146e4c41ceeb7ac43b
Mariacos <mariacos@student.42.lisboa.com>
2026-08-14 20:05:32 +0100

    ex01 not done yet

---
+++ cpp01/ex01/srcs/Zombie.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariacos <mariacos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/13 20:20:18 by mariacos          #+#    #+#             */
+/*   Updated: 2026/08/14 18:17:38 by mariacos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie::Zombie() 
+{
+    std::cout << "\033[33mZombie " << _name << " created! Yay!\033[0m" << std::endl;
+}
+
+Zombie::~Zombie(void) 
+{
+    std::cout << "\033[31mZombie " << _name << " destrrrroooyeddd! Muahahahahah!\033[0m" << std::endl;
+}
+
+void Zombie::announce(void)
+{
+    std::cout << "\033[34m" << this->_name << ": BraiiiiiiinnnzzzZ..." << "\033[0m" << std::endl;
+}
+
+void Zombie::setName(const std::string &name)
+{
+    this->_name = name;
+}
+
+const std::string &Zombie::getName() const
+{
+    return (this->_name);
+}
---
+++ cpp01/ex01/srcs/ZombieHorde.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ZombieHorde.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariacos <mariacos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/08/14 17:20:12 by mariacos          #+#    #+#             */
+/*   Updated: 2026/08/14 17:34:01 by mariacos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+Zombie* zombieHorde( int N, std::string name )
+{
+    // allocate N zombie objects in a single allocation
+    // initialize all of the zombies by giving the name var
+    // function returns pointer to first zombie
+    Zombie* horde = new Zombie[N];
+    
+};
